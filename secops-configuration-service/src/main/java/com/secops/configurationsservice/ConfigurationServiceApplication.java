c53a3cfae53c50ac9e82e1b78252e10447e00fba
Rohit Gupta <rohit_gupta@symantec.com>
2019-04-27 20:32:07 +0530

    Enable Config Server and add config server dependencies. Add bootstrap properties for config server.

--- secops-configuration-service/src/main/java/com/secops/configurationsservice/ConfigurationServiceApplication.java
+++ secops-configuration-service/src/main/java/com/secops/configurationsservice/ConfigurationServiceApplication.java
@@ -2,8 +2,10 @@ package com.secops.configurationsservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class ConfigurationServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(ConfigurationServiceApplication.class, args);

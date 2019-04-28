4520d543dc838a64aab4616b7a45b373e6b110bc
Rohit Gupta <rohit_gupta@symantec.com>
2019-04-28 10:31:24 +0530

    Add Eureka Client for service discovery

--- secops-configuration-service/src/main/java/com/secops/configurationsservice/ConfigurationServiceApplication.java
+++ secops-configuration-service/src/main/java/com/secops/configurationsservice/ConfigurationServiceApplication.java
@@ -2,10 +2,12 @@ package com.secops.configurationsservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer
+@EnableDiscoveryClient
 public class ConfigurationServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(ConfigurationServiceApplication.class, args);

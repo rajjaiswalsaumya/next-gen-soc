cf1df030b6f9dbcc3b9bf6e6fe9db96bd5ed9e13
Rohit Gupta <rohit_gupta@symantec.com>
2019-05-04 22:38:20 +0530

    Adding angular-bootstrap-md

---
+++ secops-ui/src/app/ui.module.ts
@@ -0,0 +1,10 @@
+import {NgModule} from '@angular/core';
+import {MDBBootstrapModule} from 'angular-bootstrap-md';
+
+@NgModule({
+  imports: [
+    MDBBootstrapModule.forRoot()
+  ]
+})
+export class UIModule {
+}
